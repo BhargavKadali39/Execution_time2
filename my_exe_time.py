f54edee26d9214eb32413b1c36919df11104a4b9
Bhargav <bhargavkadali39@gmail.com>
2021-10-09 23:28:43 +0530

    Update my_exe_time.py

--- my_exe_time.py
+++ my_exe_time.py
@@ -1,1 +1,13 @@
 
+import timeit
+
+code_to_test = """
+a = range(100000)
+b = []
+for i in a:
+    b.append(i*2)
+"""
+
+elapsed_time = timeit.timeit(code_to_test, number=500)
+print(elapsed_time)
+# 10.159821493085474
